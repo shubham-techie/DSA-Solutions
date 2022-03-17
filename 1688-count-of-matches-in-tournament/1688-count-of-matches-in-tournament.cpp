469f64ab1ebcb4c765924c50efe809e3a4b3097e
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-17 22:01:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.68%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        
-        int match{};
+        return n-1;
        /* 
-        while(n!=1){
-            if(n%2){
-                --n;
-                n>>=1;
-                match+=n;
-                n+=1;
-            }
-            else{
-                n>>=1;
-                match+=n;
-            }  
-        }
-        */
-        
         while(n>1){
             match+=n>>1;
             n=(n+1)>>1;
         }
         return match;
+        */
     }
 };
