5004a33d866caedbcf82a2247013bbf5b98d6f3b
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-18 14:34:50 +0530

    Time: 21 ms (10.79%), Space: 9.6 MB (57.63%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int a[32]{0};
-        
-        for(auto& it:nums){
-            bitset<32> b(it);
-            
-            for(int j=0;j<32;++j)
-                a[j]=(a[j]+b[j])%3;
-        }
-        
-        long long base=1;
         int dec{};
         
-        for(int i=0;i<32;++i){
-            dec+=a[i]*base;
-            base*=2;
+        for(int i=31;i>=0;--i){
+            int cnt{};
+            int mask=1<<i;
+            
+            for(int num:nums){
+                if(num & mask)
+                ++cnt;
+            }
+            
+            if(cnt%3)
+                dec|=(1<<i);
         }
+
+//         int a[32]{0};
+        
+//         for(auto& it:nums){
+//             bitset<32> b(it);
+            
+//             for(int j=0;j<32;++j)
+//                 a[j]=(a[j]+b[j])%3;
+//         }
+        
+//         long long base=1;
+        
+//         for(int i=0;i<32;++i){
+//             dec+=a[i]*base;
+//             base*=2;
+//         }
         return dec;
     }
 };
