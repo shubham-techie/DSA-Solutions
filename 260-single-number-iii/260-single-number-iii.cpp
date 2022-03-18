9573e10997beaf7439ad5783e8632a60ae237de2
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-18 13:09:17 +0530

    Time: 4 ms (98.67%), Space: 10 MB (78.73%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-       vector<int> v(2,1);
-        /*
-        unordered_map<int,int> map;
-        int i=-1;
         
-        for(auto& it:nums)
-            ++map[it];
-        
-        for(auto& it:map){
-            if(it.second==1)
-                v[++i]=it.first;
-        }
-        */
         int xorr{};
         int n1{};
         int idx{};
-        int tmp{};
         
         for(auto& it:nums)
             xorr^=it;
         
-        tmp=xorr;
+        int tmp=xorr;
         
-        while(1){
+        while(1)
             if(tmp & 1)
                 break;
             else{
                 tmp>>=1;
                 ++idx;
             }
-        }
         
         int mask=1<<idx;
-        
-        for(auto& it:nums){
+        for(auto& it:nums)
             if(it & mask)
                 n1^=it;  
-        }
-        
-        xorr^=n1;
-        
-        v={xorr,n1};
-         return v;
+
+         return vector<int>{xorr^n1,n1};
     }
 };
