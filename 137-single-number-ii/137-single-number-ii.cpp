05f7f9bb680d73af9c80fac49f8023edf168f28a
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-18 14:35:49 +0530

    Time: 11 ms (60.93%), Space: 9.5 MB (57.63%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -3,34 +3,34 @@ public:
     int singleNumber(vector<int>& nums) {
         int dec{};
         
-        for(int i=31;i>=0;--i){
-            int cnt{};
-            int mask=1<<i;
+//         for(int i=31;i>=0;--i){
+//             int cnt{};
+//             int mask=1<<i;
             
-            for(int num:nums){
-                if(num & mask)
-                ++cnt;
-            }
+//             for(int num:nums){
+//                 if(num & mask)
+//                 ++cnt;
+//             }
             
-            if(cnt%3)
-                dec+=(1<<i);
-        }
+//             if(cnt%3)
+//                 dec+=(1<<i);
+//         }
 
-//         int a[32]{0};
+        int a[32]{0};
         
-//         for(auto& it:nums){
-//             bitset<32> b(it);
+        for(auto& it:nums){
+            bitset<32> b(it);
             
-//             for(int j=0;j<32;++j)
-//                 a[j]=(a[j]+b[j])%3;
-//         }
+            for(int j=0;j<32;++j)
+                a[j]=(a[j]+b[j])%3;
+        }
         
-//         long long base=1;
+        long long base=1;
         
-//         for(int i=0;i<32;++i){
-//             dec+=a[i]*base;
-//             base*=2;
-//         }
+        for(int i=0;i<32;++i){
+            dec+=a[i]*base;
+            base*=2;
+        }
         return dec;
     }
 };
