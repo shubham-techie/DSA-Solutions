19e5524c0424c26ef5d21c05256c946ab6838389
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-18 11:15:26 +0530

    Time: 13 ms (17.26%), Space: 7.9 MB (49.35%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,10 +3,13 @@ public:
     vector<int> countBits(int n) {
         
         vector<int> v(n+1);
+       v[0]=0;
         
-        for(int i=0;i<=n;++i){
-            bitset<32>b(i);
-            v[i]=b.count();
+        for(int i=1;i<=n;++i){
+            // bitset<32>b(i);
+            // v[i]=b.count();
+            
+            v[i]=v[i/2]+i%2;
         }
         return v;
     }
