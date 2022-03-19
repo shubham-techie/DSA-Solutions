cae19ba813d5b7810bbff067dd3f780c9965a877
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-19 11:35:49 +0530

    Time: 4 ms (73.73%), Space: 11.1 MB (30.26%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -12,11 +12,11 @@ public:
                 return mid;
             
             if(nums[mid]>=nums[l]){
-                if(target>=nums[l] && target<=nums[mid]) r=mid-1;
+                if(target>=nums[l] && target<nums[mid]) r=mid-1;
                 else l=mid+1;
             }
             else{
-                if(target>=nums[mid] && target<=nums[r]) l=mid+1;
+                if(target>nums[mid] && target<=nums[r]) l=mid+1;
                 else r=mid-1;
             }
         }
