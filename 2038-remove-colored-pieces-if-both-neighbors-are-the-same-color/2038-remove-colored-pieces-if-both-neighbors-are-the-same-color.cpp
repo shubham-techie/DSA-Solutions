74a6e94fd8ed0ba428bf7aeadb40c5ccb8366d0d
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-20 14:10:06 +0530

    Time: 32 ms (97.19%), Space: 13.4 MB (8.91%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
-        int noOfMiddleAs{}, noOfMiddleBs{};
+        int nA{}, nB{};
         char A='A',B='B';
         
     for(int i=1;i<colors.length()-1;++i)
-        if(colors[i]==A && colors[i-1]==A && colors[i+1]==A)      ++noOfMiddleAs;   
-        else if(colors[i]==B && colors[i-1]==B && colors[i+1]==B) ++noOfMiddleBs;
+        if(colors[i]==colors[i-1] && colors[i]==colors[i+1])
+            if(colors[i]==A) 
+                ++nA;
+            else
+                ++nB;
   
 
-    if(noOfMiddleAs>noOfMiddleBs) return true;
-    else return false;
+    return nA>nB;
     }
 };
