7e45d6f5a090b515f21c4246bef8f1121aac4f69
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-20 14:05:07 +0530

    Time: 36 ms (90.11%), Space: 13.4 MB (8.91%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
-        int noOfMiddleWhites{}, noOfMiddleBlacks{};
-        string s{colors};
+        int noOfMiddleAs{}, noOfMiddleBs{};
         char A='A',B='B';
         
-    for(int i=1;i<s.length()-1;++i){
-        if(s[i]==A && s[i-1]==A && s[i+1]==A){
-            ++noOfMiddleWhites;
-        }
-        else if(s[i]==B && s[i-1]==B && s[i+1]==B){
-            ++noOfMiddleBlacks;
-        }
-    }
+    for(int i=1;i<colors.length()-1;++i)
+        if(colors[i]==A && colors[i-1]==A && colors[i+1]==A)      ++noOfMiddleAs;   
+        else if(colors[i]==B && colors[i-1]==B && colors[i+1]==B) ++noOfMiddleBs;
+  
 
-    if(noOfMiddleWhites>noOfMiddleBlacks) return true;
+    if(noOfMiddleAs>noOfMiddleBs) return true;
     else return false;
     }
 };
