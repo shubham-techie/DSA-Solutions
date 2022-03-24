17b6bba6b475f866195062a5feafaa0dbf089b0b
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-24 19:28:59 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (67.86%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool squareIsWhite(string coordinates) {
-        unordered_map<char,int>map(8);
-        
-        for(char c='a';c<='h';++c)
-            map[c]=c - 'a'+1;
-        
-        int is_even=map[coordinates[0]]+(coordinates[1]-'0');
+        int is_even=coordinates[0]+coordinates[1];
 
         if( !(is_even & 1 ))
             return false;
