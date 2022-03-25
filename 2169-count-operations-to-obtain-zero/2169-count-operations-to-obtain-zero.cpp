f3b98d312105209ebc5384a90085aac3e343378b
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-25 19:27:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.77%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -3,8 +3,12 @@ public:
     int countOperations(int num1, int num2) {
         int cnt{};
         
-        while(1){
-            if(num1==0 || num2==0) break;
+        if(num2==0) return cnt;
+        
+        while(num1!=0){
+            if(num1==1) return cnt+num2;
+            if(num2==1) return cnt+num1;
+            
             if(num1>=num2) num1-=num2;
             else num2-=num1;
             ++cnt;
