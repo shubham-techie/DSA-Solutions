fc9b16865047f05b383b1e74e1d96cef0b58c187
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-03-28 20:59:45 +0530

    Time: 8 ms (59.43%), Space: 10 MB (78.57%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
+    void sortArray(vector<int>& v){
+        sort(begin(v),end(v));
+    }
+    
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> map;
+        sortArray(nums1);
+        sortArray(nums2);
+        
         vector<int> res;
         
-        for(int& it:nums1){
-            if(map[it])
-                continue;
-            ++map[it];
+        for(int i=0,j=0;i<nums1.size() && j<nums2.size();){
+            if(!(nums1[i]^nums2[j])){
+                res.emplace_back(nums1[i]);
+                ++i; ++j;
+            }
+            
+            else if(nums1[i]>nums2[j])
+                ++j;
+            
+            else
+                ++i;
+            
+            while(i>0 && i<nums1.size() && !(nums1[i-1]^nums1[i])) ++i;
+            while(j>0 && j<nums2.size() && !(nums2[j-1]^nums2[j])) ++j;
         }
         
-        for(int& it:nums2){
-            if(map[it]){
-                res.emplace_back(it);
-                --map[it];
-            }
-        }
         
         return res;
     }
