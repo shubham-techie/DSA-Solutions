1a3fe6117339ed878417073603b365b7092de531
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-01 19:32:59 +0530

    Added solution - LeetHub

--- Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.c
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.c
@@ -27,8 +27,59 @@ int main() {
 
 // User function Template for C
 #include<limits.h>
+struct pair getMnMx(long long arr[],int low,int high){
+   struct pair minmax, mml, mmr;    
+    int mid;
+     
+    // If there is only one element
+    if (low == high)
+    {
+        minmax.max = arr[low];
+        minmax.min = arr[low];    
+        return minmax;
+    }
+     
+    // If there are two elements
+    if (high == low + 1)
+    {
+        if (arr[low] > arr[high])
+        {
+            minmax.max = arr[low];
+            minmax.min = arr[high];
+        }
+        else
+        {
+            minmax.max = arr[high];
+            minmax.min = arr[low];
+        }
+        return minmax;
+    }
+     
+    // If there are more than 2 elements
+    mid = (low + high) / 2;
+    mml = getMnMx(arr, low, mid);
+    mmr = getMnMx(arr, mid + 1, high);
+     
+    // Compare minimums of two parts
+    if (mml.min < mmr.min)
+        minmax.min = mml.min;
+    else
+        minmax.min = mmr.min;    
+     
+    // Compare maximums of two parts
+    if (mml.max > mmr.max)
+        minmax.max = mml.max;
+    else
+        minmax.max = mmr.max;    
+     
+    return minmax;
+   
+}
+
 struct pair getMinMax(long long int arr[], long long int n) {
+   
    struct pair p;
+   /*
    p.min=INT_MAX;
    p.max=INT_MIN;
    
@@ -38,4 +89,8 @@ struct pair getMinMax(long long int arr[], long long int n) {
    }
    
    return p;
+   */
+   
+   //Tournament method
+   return getMnMx(arr,0,n-1);
 }
