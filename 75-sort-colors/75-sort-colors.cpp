03911677fbf05a8734422036739c4feecda9eabc
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-02 20:50:48 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.24%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int cntArray[3]{};
+       /* Method 1: Count and build array
+       
+        int cntArray[3]{},k{};
         
         for(int& i:nums)
             ++cntArray[i];
         
-        int k{};
         for(int i=0;i<nums.size();++i){
             while(!(cntArray[k]))
                 ++k;
@@ -14,8 +15,22 @@ public:
             if(cntArray[k]){
                 nums[i]=k;
                 --cntArray[k];
-            }
-            
+            } 
         }
+        */
+        
+        
+        // Method 2: Variation of Dutch National Flag Problem -- using three pointers
+        
+        int low{0},mid{0},high=nums.size()-1;
+        
+        while(mid<=high)
+            if(nums[mid]==0) 
+                swap(nums[low++],nums[mid++]);
+            else if(nums[mid]==2) 
+                swap(nums[mid],nums[high--]);
+            else mid++;
+        
+    
     }
 };
