9fe285dc724f331824cae15166ef4aacaf6f748e
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-03 21:48:34 +0530

    Time: 15 ms (18.52%), Space: 10.1 MB (79.01%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -4,7 +4,41 @@ public:
         sort(begin(v),end(v));
     }
     
+    bool binarySearch(vector<int>& v, int& x){
+        int low{},high=v.size()-1;
+        
+        while(low<=high){
+            int mid=low+ ((high-low)>>1);
+        
+        if(!(v[mid]^x))
+            return true;
+        else if(v[mid]>x)
+            high=mid-1;
+        else
+            low=mid+1;
+        }
+        return false;
+    }
+    
+    vector<int> findIntersection(vector<int>& n1,vector<int>& n2){
+        vector<int> res{};
+        int n=n2.size();
+        
+        for(int i=0;i<n;){
+            if(binarySearch(n1, n2[i]))
+                res.emplace_back(n2[i]);
+            ++i;
+            
+            while(i>0 && i<n && !(n2[i]^n2[i-1])) ++i;
+        }
+        
+        return res;
+    }
+    
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+        /* Method 1: mapping 1st array and looking for intersection by iterating 2nd array
+        
         int cntArray[1001]{};
         vector<int> res{};
         
@@ -17,6 +51,19 @@ public:
                 res.emplace_back(i);
                 --cntArray[i];
             }
+        
+        return res;
+        */
+        
+        // Method 2: Binary search
+        vector<int> res{};
+         sortArray(nums1);
+        sortArray(nums2);
+        
+        if(nums1.size()>nums2.size())
+            res=findIntersection(nums1, nums2);
+        else
+            res=findIntersection(nums2, nums1);
         
         return res;
        /*
