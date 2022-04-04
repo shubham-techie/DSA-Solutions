c90053c14b11aa98191d895ffcbc1797e881d295
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-04 21:49:30 +0530

    Time: 104 ms (90.88%), Space: 67.6 MB (99.56%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,13 +2,28 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int maxSum=nums[0];
-        int currSum{};
+        /* Method 1 : Naive approach using two pointer 
         
-        for(int i=0;i<nums.size();++i){
-            currSum+=nums[i];           
-            maxSum=max(currSum,maxSum);
-            currSum=currSum<0?0:currSum;
+        int maxSum{INT_MIN}, n=nums.size();
+        
+        for(int i=0;i<n;++i){
+            int sum{};
+            
+            for(int j=i;j<n;++j){
+                sum+=nums[j];              
+                maxSum=sum>maxSum? sum:maxSum;
+            }
+        }
+        
+        return maxSum;
+        */
+        
+        int maxSum{INT_MIN}, sum{};
+        
+        for(int& i:nums){
+            sum+=i;
+            maxSum=max(sum,maxSum);
+            sum= sum<0? 0:sum;
         }
         
         return maxSum;
