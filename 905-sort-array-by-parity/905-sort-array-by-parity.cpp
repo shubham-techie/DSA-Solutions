a264d0e2d72908a05e076d9d622de56f24d2256f
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-09 00:12:25 +0530

    Time: 11 ms (75.20%), Space: 16.3 MB (25.08%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -2,12 +2,31 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         
+        /* Method 1: Quick select : partition method
+        
         int i{};
         for(int& j:nums){
             if((j&1)==0){
                 swap(nums[i],j);
                 ++i;
             }
+        }
+        
+        return nums;
+        */
+        
+        // Method 2: two pointers
+        int l{},h=nums.size()-1;
+        
+        while(l<h){
+            if((nums[l]&1) & !(nums[h]&1)){
+                swap(nums[l],nums[h]);
+                ++l;
+                --h;
+            }
+            
+            else if(!(nums[l]&1))  ++l;
+            else if(nums[h]&1)     --h;
         }
         
         return nums;
