872d1f40bf7ce839c42a25879685046c34d1972f
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-12 20:31:53 +0530

    Time: 10 ms (6.86%), Space: 9.1 MB (73.08%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
-        
+        /* O(n*m)
         if(n){
             int i{m-1};
 
-            for(int j=0;j<n;++j){
+            for(int j=n-1;j>=0;--j){
                 int i{m-1};
 
                 while(i>=0 && v1[i]>v2[j])
@@ -18,6 +18,28 @@ public:
 
                 v1[i]=v2[j];
             }
+        }
+        */
+        
+        int i{m-1},j{n-1},k{m+n-1};
+        
+        while(i>=0 && j>=0){
+            if(v2[j]>=v1[i]){
+                v1[k]=v2[j];
+                --k;
+                --j;
+            }
+            else if(v1[i]>v2[j]){
+                v1[k]=v1[i];
+                --k;
+                --i;
+            }
+        }
+       
+        while(j>=0){
+            v1[k]=v2[j];
+            --k;
+            --j;
         }
     }
 };
