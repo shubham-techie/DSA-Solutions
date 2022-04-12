24fdcd4681594efcfcf755f8ec21b8f2504dc1f1
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-12 20:43:48 +0530

    Time: 5 ms (30.79%), Space: 9 MB (90.53%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -23,23 +23,7 @@ public:
         
         int i{m-1},j{n-1},k{m+n-1};
         
-        while(i>=0 && j>=0){
-            if(v2[j]>=v1[i]){
-                v1[k]=v2[j];
-                --k;
-                --j;
-            }
-            else if(v1[i]>v2[j]){
-                v1[k]=v1[i];
-                --k;
-                --i;
-            }
-        }
-       
-        while(j>=0){
-            v1[k]=v2[j];
-            --k;
-            --j;
-        }
+        while(j>=0)
+            v1[k--] = (i>=0 && v1[i]>=v2[j])? v1[i--]:v2[j--];
     }
 };
