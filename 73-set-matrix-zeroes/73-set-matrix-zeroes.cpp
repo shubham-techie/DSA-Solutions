741724c00bd3dda08b12991f80da29ca9aa7a5e3
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-18 01:27:04 +0530

    Time: 20 ms (48.36%), Space: 13 MB (95.99%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,34 +2,28 @@ class Solution {
 public:
  
     void setZeroes(vector<vector<int>>& v) {
-//        // SC: O(m+n), TC: O(3*(m*n))
+
+        /* SC: O(m+n), TC: O(2*(m*n))
        
-//         set<int> r{},c{};
+        set<int> r{},c{};
         
-//         int m=v.size(), n=v[0].size();
+        int m=v.size(), n=v[0].size();
         
-//         for(int i=0;i<m;++i)
-//             for(int j=0;j<n;++j)
-//                 if(!(v[i][j]^0)){
-//                     r.insert(i);
-//                     c.insert(j);
-//                 }
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(!(v[i][j]^0)){
+                    r.insert(i);
+                    c.insert(j);
+                }
         
-//         /* with this approach --> TC: O(3*(m*n))
-//         for(int i:r)
-//             for(int j=0;j<n;++j)
-//                 v[i][j]=0;
+          for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(r.find(i)!=r.end() || c.find(j)!=c.end())
+                    v[i][j]=0;  
+        */
         
-//         for(int i:c)
-//             for(int j=0;j<m;++j)
-//                 v[j][i]=0; 
-//         */
         
-//         //Alternative to above approach --> TC: O(2*(m*n))
-//           for(int i=0;i<m;++i)
-//             for(int j=0;j<n;++j)
-//                 if(r.find(i)!=r.end() || c.find(j)!=c.end())
-//                     v[i][j]=0;
+        /* SC: O(m+n), TC: O(2*(m*n))
         
         int m=v.size(), n=v[0].size();
         vector<int> r(m,0), c(n,0);
@@ -45,5 +39,37 @@ public:
             for(int j=0;j<n;++j)
                 if(!(r[i]^1) || !(c[j]^1))
                     v[i][j]=0;
+        */
+        
+        
+        int m=v.size(), n=v[0].size(), col0{1},row0{1};
+        
+        for(int i=0;i<n;++i)
+            if(!(v[0][i]^0))
+                row0=0;
+        
+          for(int i=0;i<m;++i)
+             if(!(v[i][0]^0))
+                 col0=0;
+        
+        for(int i=1;i<m;++i)
+            for(int j=1;j<n;++j)
+                if(!(v[i][j]^0)){
+                    v[i][0]=0;
+                    v[0][j]=0;
+                }
+        
+        for(int i=m-1;i>0;--i)
+            for(int j=n-1;j>0;--j)
+                if(!(v[i][0]^0) || !(v[0][j]^0))
+                    v[i][j]=0;
+        
+        for(int i=0;i<n;++i)
+            if(!(row0^0))
+                v[0][i]=0;
+        
+        for(int i=0;i<m;++i)
+            if(!(col0^0))
+                v[i][0]=0;
     }
 };
