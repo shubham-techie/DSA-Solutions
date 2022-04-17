6eaa3e77df268d31181895115ea3d3b8b634e759
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-17 21:45:06 +0530

    Time: 12 ms (91.13%), Space: 13.2 MB (54.55%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,32 +2,48 @@ class Solution {
 public:
  
     void setZeroes(vector<vector<int>>& v) {
-       // SC: O(m+n), TC: O(3*(m*n))
+//        // SC: O(m+n), TC: O(3*(m*n))
        
-        set<int> r{},c{};
+//         set<int> r{},c{};
+        
+//         int m=v.size(), n=v[0].size();
+        
+//         for(int i=0;i<m;++i)
+//             for(int j=0;j<n;++j)
+//                 if(!(v[i][j]^0)){
+//                     r.insert(i);
+//                     c.insert(j);
+//                 }
+        
+//         /* with this approach --> TC: O(3*(m*n))
+//         for(int i:r)
+//             for(int j=0;j<n;++j)
+//                 v[i][j]=0;
+        
+//         for(int i:c)
+//             for(int j=0;j<m;++j)
+//                 v[j][i]=0; 
+//         */
+        
+//         //Alternative to above approach --> TC: O(2*(m*n))
+//           for(int i=0;i<m;++i)
+//             for(int j=0;j<n;++j)
+//                 if(r.find(i)!=r.end() || c.find(j)!=c.end())
+//                     v[i][j]=0;
+        
         int m=v.size(), n=v[0].size();
+        vector<int> r(m,0), c(n,0);
         
         for(int i=0;i<m;++i)
             for(int j=0;j<n;++j)
                 if(!(v[i][j]^0)){
-                    r.insert(i);
-                    c.insert(j);
+                    r[i]=1;
+                    c[j]=1;
                 }
         
-        /*
-        for(int i:r)
+        for(int i=0;i<m;++i)
             for(int j=0;j<n;++j)
-                v[i][j]=0;
-        
-        for(int i:c)
-            for(int j=0;j<m;++j)
-                v[j][i]=0; 
-        */
-        
-        //Alternative to above approach
-          for(int i=0;i<m;++i)
-            for(int j=0;j<n;++j)
-                if(r.find(i)!=r.end() || c.find(j)!=c.end())
+                if(!(r[i]^1) || !(c[j]^1))
                     v[i][j]=0;
     }
 };
