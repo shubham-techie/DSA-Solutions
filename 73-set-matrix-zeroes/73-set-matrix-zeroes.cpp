78c15ce701895ef269806b2e89d8bbdc93734518
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-17 21:36:53 +0530

    Time: 18 ms (58.44%), Space: 13.4 MB (11.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
+ 
     void setZeroes(vector<vector<int>>& v) {
+       // SC: O(m+n), TC: O(3*(m*n))
+       
         set<int> r{},c{};
         int m=v.size(), n=v[0].size();
         
@@ -11,12 +14,20 @@ public:
                     c.insert(j);
                 }
         
+        /*
         for(int i:r)
             for(int j=0;j<n;++j)
                 v[i][j]=0;
         
         for(int i:c)
             for(int j=0;j<m;++j)
-                v[j][i]=0;
+                v[j][i]=0; 
+        */
+        
+        //Alternative to above approach
+          for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(r.find(i)!=r.end() || c.find(j)!=c.end())
+                    v[i][j]=0;
     }
 };
