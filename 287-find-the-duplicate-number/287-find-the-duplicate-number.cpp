8d9e0afd573c1828718a47dbfc4798b7ad53fa3e
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-21 15:42:07 +0530

    Time: 133 ms (70.27%), Space: 61.2 MB (61.90%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
-        /*
+        
+        /* Method 2 : Swapping numbers until all elements-1 are in its place equal to indices and then only last index will be left which have repeating element.
+        
        for(int i=0;i<n;)
            if(nums[i]!=nums[nums[i]-1])
                swap(nums[i],nums[nums[i]-1]);
@@ -12,11 +14,21 @@ public:
         return nums[n-1];
         */
         
+        /* Method 2: frequency count 
         for(int i=0;i<n;++i){
             nums[nums[i]%n]+=n;
             if(nums[nums[i]%n]>2*n)
                 return nums[i]%n;
         }
-        return -1;
+        return 0;
+        */
+        
+        for(int i=0;i<n;++i){
+            if(nums[abs(nums[i])]<0)
+                return abs(nums[i]);
+            nums[abs(nums[i])]*=-1;
+        }
+        
+        return 0;
     }
 };
