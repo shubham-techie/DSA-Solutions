b8a54219a994023d61de4eb3c53b9258c5c1cb8d
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-21 22:10:32 +0530

    Time: 228 ms (21.12%), Space: 61.2 MB (91.74%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -3,7 +3,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
         
-        /* Method 2 : Swapping numbers until all elements-1 are in its place equal to indices and then only last index will be left which have repeating element.
+        /* Method 1 : Swapping numbers until all elements-1 are in its place equal to indices and then only last index will be left which have repeating element.
         
        for(int i=0;i<n;)
            if(nums[i]!=nums[nums[i]-1])
@@ -14,7 +14,7 @@ public:
         return nums[n-1];
         */
         
-        /* Method 2: frequency count 
+        /* Method 2 : frequency count 
         for(int i=0;i<n;++i){
             nums[nums[i]%n]+=n;
             if(nums[nums[i]%n]>2*n)
@@ -23,6 +23,8 @@ public:
         return 0;
         */
         
+        /* Method 3: Negating the value at index and if again that element would be repaeting then, the element at that index would be <0.
+        
         for(int i=0;i<n;++i){
             if(nums[abs(nums[i])]<0)
                 return abs(nums[i]);
@@ -30,5 +32,24 @@ public:
         }
         
         return 0;
+        */
+        
+        // Method 4: Linked list cycle method
+        
+        int slow{nums[0]},fast=nums[0];
+        
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+          }while(slow^fast);
+        
+        fast=nums[0];
+        
+        while(slow^fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        
+        return fast;
     }
 };
