2cf229cfd083000d680fdfb8e6e984fd25d622e0
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-21 20:12:02 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -24,15 +24,38 @@ class Solution{
             //     arr2[j-1]=tmp;
             // }
             
-            
-            for(int i{n-1}, j{};i>=0 && j<m;--i,++j){
+            /*
+            for(int i{n-1}, j{};i>=0 && j<m;--i,++j)
                 if(arr1[i]>arr2[j])
                     swap(arr1[i],arr2[j]);
-            }
+            
             
             sort(arr1,arr1+n);
             sort(arr2,arr2+m);
+            */
             
+            //Gap method
+            int gap=ceil(m+n)/2.0;
+            
+            while(gap>0){
+                
+                for(int i{},j{gap};j<(m+n);++i,++j){
+                    
+                    if(j<n && arr1[i]>arr1[j])
+                        swap(arr1[i],arr1[j]);
+                        
+                    else if(j>=n && i<n && arr1[i]>arr2[j-n])
+                        swap(arr1[i],arr2[j-n]);
+                        
+                    else if(j>=n && i>=n && arr2[i-n]>arr2[j-n])
+                        swap(arr2[i-n],arr2[j-n]);
+                }
+              
+                if (gap == 1) 
+                    gap = 0;
+                else 
+                    gap = ceil((float) gap / 2);
+            }
         } 
 };
 
