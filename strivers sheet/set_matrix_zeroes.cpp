e25c0ec32c0fceecd9b5523de3c1f80a541fd9d8
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-22 21:35:05 +0530

    Create set_matrix_zeroes.cpp

---
+++ strivers sheet/set_matrix_zeroes.cpp
@@ -0,0 +1,72 @@
+[https://leetcode.com/problems/set-matrix-zeroes/]
+
+class Solution {
+public:
+ 
+    void setZeroes(vector<vector<int>>& v) {
+
+        /* SC: O(m+n), TC: O(2*(m*n))
+       
+        set<int> r{},c{};
+        
+        int m=v.size(), n=v[0].size();
+        
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(!(v[i][j]^0)){
+                    r.insert(i);
+                    c.insert(j);
+                }
+        
+          for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(r.find(i)!=r.end() || c.find(j)!=c.end())
+                    v[i][j]=0;  
+        */
+        
+        
+        /* SC: O(m+n), TC: O(2*(m*n))
+        
+        int m=v.size(), n=v[0].size();
+        vector<int> r(m,0), c(n,0);
+        
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(!(v[i][j]^0)){
+                    r[i]=1;
+                    c[j]=1;
+                }
+        
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(!(r[i]^1) || !(c[j]^1))
+                    v[i][j]=0;
+        */
+        
+        
+        // TC: O(2* n*m) , SP: O(1)
+        int m=v.size(), n=v[0].size(), col0{1};
+    
+        for(int i=0;i<m;++i){
+            
+            if(!(v[i][0]^0))
+                 col0=0;
+            
+            for(int j=1;j<n;++j)
+                if(!(v[i][j]^0)){
+                    v[i][0]=0;
+                    v[0][j]=0;
+                }
+        }
+        
+        for(int i=m-1;i>=0;--i){
+            for(int j=n-1;j>=1;--j)
+                if(!(v[i][0]^0) || !(v[0][j]^0))
+                    v[i][j]=0;
+            
+             if(!(col0^0))
+                v[i][0]=0;
+        }
+   
+    }
+};
