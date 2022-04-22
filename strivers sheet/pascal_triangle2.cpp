a4cd934037191c7242807b375bcde32c620be24f
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-22 21:33:34 +0530

    Create pascal_triangle2.cpp

---
+++ strivers sheet/pascal_triangle2.cpp
@@ -0,0 +1,16 @@
+[https://leetcode.com/problems/pascals-triangle-ii/]
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        long tmp{1};
+        vector<int> v(rowIndex+1,1);
+        
+        for(int i=1,up=rowIndex,down=1;i<rowIndex;++i,--up,++down)
+            v[i]=tmp=tmp*up/down;
+        
+        
+        return v;
+    }
+};
