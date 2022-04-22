8f931cba3a8202499fa069a0e9a434a41084b7be
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-22 22:31:46 +0530

    Time: 5 ms (50.59%), Space: 9.4 MB (95.73%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
+    bool bsearch(vector<int>& it,int& t){
+        
+         int n=it.size(),l{}, h{n-1};
+
+        while(l<=h){
+            int m=(l+h)/2;
+
+            if(!(it[m]^t))
+                return true;
+            if(it[m]>t)
+                h=m-1;
+            else if(it[m]<t)
+                l=m+1;
+        }
+        return false;
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int t) {
         
         int m=matrix.size(), n=matrix[0].size();
         
+        /*
         for(auto& it:matrix){
             
             int l{}, h{n-1};
@@ -18,6 +36,29 @@ public:
                 else if(it[m]<t)
                     l=m+1;
             }
+        }
+        
+        return false;
+        */
+        
+        if(t<matrix[0][0] || t>matrix[m-1][n-1])
+            return false;
+        
+        int low_r{},high_r{m-1};
+        
+        while(low_r<=high_r){
+            
+            int mid_r=(low_r+high_r)/2;
+            auto& it=matrix[mid_r];
+            
+            if(it[0]<=t && t<=it[n-1]) 
+                return bsearch(it,t);
+            
+            else if(t<it[0])
+                high_r=mid_r-1;
+            
+            else if(t>it[n-1])
+                low_r=mid_r+1;
         }
         
         return false;
