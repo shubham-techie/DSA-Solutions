fdb7ce65b7812906bc41d18291a9b7edaf78dedf
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-27 21:28:25 +0530

    Time: 18 ms (83.71%), Space: 19.7 MB (18.66%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -4,8 +4,8 @@ public:
         int ele{}, cnt{};
         
         for(int& i:nums){
-            ele=!cnt? i : ele;
-            cnt=(i^ele)? --cnt : ++cnt;
+            ele=(cnt==0)? i : ele;
+            cnt=(i==ele)? --cnt : ++cnt;
         }
         return ele;
     }
