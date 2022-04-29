9e9b7656243e282313a441e781c108b6effa8104
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-04-29 12:03:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.39%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -9,7 +9,17 @@ public:
     }
     
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return fibo(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return fibo(n,dp);
+        
+        if(n==0) return 0;
+        int curr{1},prev1{1},prev2{0};
+        
+        for(int i=2;i<=n;++i){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
     }
 };
