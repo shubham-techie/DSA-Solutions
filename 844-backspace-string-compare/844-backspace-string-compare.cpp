b89c3703335bef46cbd430ac48f8dfc7f944e278
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-01 18:00:40 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (39.15%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -3,16 +3,32 @@ public:
     void processString(string& s, int& k){
         
         for(int i=0;i<s.length();++i)
-            if(s[i]!='#'){
+            if(s[i]!='#')
                 s[k++]=s[i];
-            }
             else{
                 --k;
                 k=max(0,k);
             }
     }
     
+    string processString(string s){
+        string res="";
+        int bs{};
+        
+        for(int i=s.length()-1; i>=0;--i)
+            if(s[i]=='#')
+                ++bs;
+            else{
+                if(bs)
+                    --bs;
+                else
+                    res+=s[i];
+            }
+        return res;
+    }
+    
     bool backspaceCompare(string s, string t) {
+        /*
         int sn{}, tn{};
         
         processString(s, sn);
@@ -25,5 +41,10 @@ public:
             if(s[i]!=t[i])
                 return false;
         return true;
+        */
+        
+        s=processString(s);
+        t=processString(t);
+        return s==t;
     }
 };
