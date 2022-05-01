3d7006f199949db7713836bd765d6ea67eaf6b11
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-02 01:41:48 +0530

    Time: 367 ms (17.75%), Space: 20.6 MB (50.42%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        /*
         int n=nums.size();
         vector<vector<int>> res{};
         
-        if(n<2)
-            return res;
+        if(n<3) return res;
         
         sort(nums.begin(),nums.end());
-        
-        if(nums[0]>0)
-            return res;
+        if(nums[0]>0) return res;
         
         for(int i=0;i<n-2;++i){
             
-            if(nums[i]>0)
-                break;
+            if(nums[i]>0) break;
             
-            if(i==0 || nums[i]!=nums[i-1]){
+            else if(i==0 || nums[i]!=nums[i-1]){
+            
                 int l{i+1}, r{n-1}, target=-nums[i];
 
                 while(l<r)
@@ -36,6 +33,37 @@ public:
                         --r;
             }
         }
+        return res;
+        */
+        
+        
+        int n=nums.size();
+        vector<vector<int>> res{};
+        
+         if(n<3) return res;
+        
+        sort(nums.begin(),nums.end());
+        if(nums[0]>0) return res;
+        
+        unordered_map<int,int> map;
+        for(int i=0;i<n;++i)
+            map[nums[i]]=i;
+        
+        for(int i=0;i<n-2;++i){
+
+            if(nums[i]>0) break;
+            
+            for(int j=i+1;j<n-1;++j){
+                int target= -nums[i]-nums[j];
+                
+                if(map.count(target) && map[target]>j)
+                    res.push_back({nums[i],nums[j],target});
+                
+                j=map[nums[j]];
+            }
+            i=map[nums[i]];
+        }
+        
         return res;
     }
 };
