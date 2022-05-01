dd163ba957d112235103ced163d6e8b1fa484b01
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-01 21:09:10 +0530

    Time: 225 ms (24.66%), Space: 14.9 MB (69.15%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
+    bool bSearch(vector<int>& v, const int& target){
+        int n=v.size(), l{}, r{n-1};
+            
+        while(l<=r){
+            int mid=(l+r)/2;
+
+            if(v[mid]==target)
+                return true;
+            if(v[mid]>target)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return false;
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix[0].size();
         
-        for(auto& v:matrix){
-            int l{}, r{n-1};
-            
-            while(l<=r){
-                int mid=(l+r)/2;
-                
-                if(v[mid]==target)
-                    return true;
-                if(v[mid]>target)
-                    r=mid-1;
-                else
-                    l=mid+1;
-            }
-        }
+        for(auto& v:matrix)
+          if(target>=v[0] && target<=v[n-1] && bSearch(v,target))
+              return true;
+        
         return false;
+        
+        
+   
     }
 };
