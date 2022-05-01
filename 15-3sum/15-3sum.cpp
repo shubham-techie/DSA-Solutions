5b12341878eff6d25cb42d286d4d93b458d0f183
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-02 00:31:21 +0530

    Time: 99 ms (70.30%), Space: 20 MB (68.14%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        vector<vector<int>> res; 
-        sort(num.begin(), num.end()); 
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> res{};
         
-        for (int i = 0; i < (int)(num.size())-2; i++) {
-            
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                
-                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
-                
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        
-                        vector<int> temp; 
-                        temp.push_back(num[i]); 
-                        temp.push_back(num[lo]); 
-                        temp.push_back(num[hi]); 
-                        res.push_back(temp);
-                        
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;
-                        
-                        lo++; hi--;
-                    } 
-                    else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
-               }
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n-2;++i){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int l{i+1}, r{n-1}, target=-nums[i];
+
+                while(l<r)
+                    if(nums[l]==target-nums[r]){
+                        res.insert(res.end(), {nums[l],nums[r],nums[i]});
+
+                        while(l<r && nums[l]==nums[l+1]) ++l;
+                        while(l<r && nums[r]==nums[r-1]) --r;
+                        ++l;
+                        --r;
+                    }
+                    else if(nums[l]<target-nums[r])
+                        ++l;
+                    else
+                        --r;
             }
         }
         return res;
