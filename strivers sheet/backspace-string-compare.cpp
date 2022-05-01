b1b5c196e88fdad3c7420c61e4b63342feb0244e
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-01 19:33:07 +0530

    Create backspace-string-compare.cpp

---
+++ strivers sheet/backspace-string-compare.cpp
@@ -0,0 +1,64 @@
+[https://leetcode.com/problems/backspace-string-compare/]
+
+class Solution {
+public:
+    void processString1(string& s){
+        int k{};
+        
+        for(int i=0;i<s.length();++i)
+            if(s[i]!='#')
+                s[k++]=s[i];
+            else{
+                --k;
+                k=max(0,k);
+            }
+        
+        s=s.substr(0,k);
+    }
+    
+    string processString2(string s){
+        string res="";
+        int bs{};
+        
+        for(int i=s.length()-1; i>=0;--i)
+            if(s[i]=='#')
+                ++bs;
+            else{
+                if(bs) --bs;
+                else   res+=s[i];
+            }
+        return res;
+    }
+    
+    stack<char> processString3(string s){
+        stack<char> s1;
+        
+        for(auto& i:s)
+            if(i=='#' && !s1.empty())
+                s1.pop();
+            else if(i!='#')
+                s1.push(i);
+        return s1;
+    }
+    
+    bool backspaceCompare(string s, string t) {
+        
+        //TC: O(3n) | SC:O(1)
+        processString1(s);
+        processString1(t);
+        return s==t;
+        
+        
+        /* TC: O(3n) | SC:O(n)
+        s=processString2(s);
+        t=processString2(t);
+        return s==t;
+        */
+        
+        /* //TC: O(3n) | SC:O(2n)
+        stack<char> s1=processString3(s);
+        stack<char> t1=processString3(t);
+        return s1==t1;
+        */
+    }
+};
