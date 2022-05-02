de2756e88dca80eb6bd255fad913073795f8c6cc
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-02 19:53:27 +0530

    Time: 1271 ms (5.88%), Space: 30.6 MB (68.48%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -12,34 +12,26 @@ public:
             if(nums[i]-1 ==nums[i-1]){
                 ++tmp_max;
                 mx=max(mx, tmp_max);
-                
             }
             else if(nums[i]!=nums[i-1])
                 tmp_max=1;
-            
         }
     
         return mx;
         */
         
         unordered_set<int> set{};
-        int mxStreak{};
+        int mx{};
         
         for(int& i:nums)
             set.insert(i);
         
         for(int& i:nums)
             if(!set.count(i-1)){
-                int currStreak=1;
-                int currNum=i+1;
-
-                while(set.count(currNum)){
-                    ++currStreak;
-                    ++currNum;
-                }
-                mxStreak=max(mxStreak,currStreak);
+                int cur=1;
+                while(set.count(i+cur)) ++cur;
+                mx=max(mx,cur);
             }
-        
-        return mxStreak;
+        return mx;
     }
 };
