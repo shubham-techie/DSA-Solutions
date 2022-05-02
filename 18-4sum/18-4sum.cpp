1ed1d984c0cc1eff350b88a230d0d87789045652
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-02 16:40:46 +0530

    Time: 131 ms (35.41%), Space: 13.1 MB (90.73%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -10,15 +10,9 @@ public:
         vector<vector<int>> res{};
         
         for(int i=0;i<n-3;++i){
-            
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
-            
+       
             for(int j=i+1;j<n-2;++j){
-                
-                if(j>i+1 && nums[j]==nums[j-1])
-                    continue;
-                
+  
                 long long new_target=target-nums[i]-nums[j];
                 int l{j+1}, r{n-1};
                 
@@ -37,7 +31,10 @@ public:
                     else
                         --r;
                 }
+                
+                while(j<n-1 && nums[j]==nums[j+1]) ++j;
             }
+            while(i<n-1 && nums[i]==nums[i+1]) ++i;
         }
         
         return res;
