5b96add1030f35032a77d40fb9e2c9a98c9b3059
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-04 22:02:55 +0530

    Time: 14 ms (72.91%), Space: 18.3 MB (34.94%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -8,11 +8,9 @@ public:
         maxrhs[n-1]=height[n-1];
         maxlhs[0]=height[0];
         
-        int i{1};
-        while(i<n){
+        for(int i=1;i<n;++i){
             maxlhs[i]=max(maxlhs[i-1],height[i]);
             maxrhs[n-1-i]=max(maxrhs[n-i],height[n-1-i]);
-            ++i;
         }
        
         int sum{};
