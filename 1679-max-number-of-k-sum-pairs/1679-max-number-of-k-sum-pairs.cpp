33c1901488fc8d3f6cf1ba64394cc894d7687cd6
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-05 09:45:09 +0530

    Time: 213 ms (40.70%), Space: 67.7 MB (43.82%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -15,36 +15,36 @@ public:
         */
         
         
-        /* M2: mapping count | TC:O(n) | SC:O(n)
+        // M2: mapping count | TC:O(n) | SC:O(n)
         unordered_map<int,int> map{};
         int n=nums.size(), cnt{};
         
         for(int i:nums)
             ++map[i];
-        
+                
         for(auto& [f,s]:map){
             if(f*2==k){
                 cnt+=s/2;
                 s=0;
             }
-            else if(s>0 && map.count(k-f)){
+            else if(s>0 && map.count(k-f) && map[k-f]>0){
                 cnt+=min(s, map[k-f]);
-                
                 map[k-f]=0;
                 s=0;
             }
         }
         return cnt;
-        */
         
         
+        /* M3 : Mapping and one pass | TC:O(n) | SC:O(n)
         int cnt{};
         unordered_map<int,int> map{};
         
-        for(int& i:nums){
+        for(int& i:nums)
             if(map[k-i]>0) ++cnt, --map[k-i];
             else ++map[i];
-        }
+        
         return cnt;
+        */
     }
 };
