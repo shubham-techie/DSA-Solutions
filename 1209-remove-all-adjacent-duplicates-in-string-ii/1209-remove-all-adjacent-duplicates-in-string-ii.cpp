948d9d9d99618716c4a98e24101f1a00c0ed3733
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-07 01:29:37 +0530

    Time: 15 ms (93.44%), Space: 11 MB (30.08%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
+        /*
         int n=s.length();
         if(n<k) return s;
         vector<pair<char,int>> stk{};
@@ -19,5 +20,25 @@ public:
             s+=string(tmp.second, tmp.first);
         
          return s;
+        */
+        
+        int n=s.length(), j{};
+        if(n<k) return s;
+        vector<int> cnt(n);
+        
+        for(int i=0;i<n;++i,++j){
+            s[j]=s[i];
+            
+            if(j>0 && s[j]==s[j-1])
+                cnt[j]=cnt[j-1]+1;
+            
+            else
+                cnt[j]=1;
+            
+            if(cnt[j]==k)
+                j=j-k;
+        }
+        
+        return s.substr(0,j);
     }
 };
