0f266b774475dd89d0ddc7b56bc98e4ad0c7ad38
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-11 19:12:17 +0530

    Time: 1 ms (78.62%), Space: 43.5 MB (32.34%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -2,7 +2,6 @@ class Solution {
     List<List<Integer>> res=new ArrayList<>();
     
     void backtrack(int[] nums, int start, LinkedList<Integer> l){
-        
         if(start==nums.length){
             res.add(new ArrayList(l));
             return;
@@ -15,10 +14,27 @@ class Solution {
         backtrack(nums, start+1, l);
     }
     
-    public List<List<Integer>> subsets(int[] nums) {
-        
+    public List<List<Integer>> subsets(int[] nums) { 
+        /*
         backtrack(nums, 0, new LinkedList<Integer>());
-        
         return res;
+        */
+        
+        
+        List<List<Integer>> resu=new ArrayList<>();
+        resu.add(new ArrayList<Integer>());
+        
+        for(int j : nums){
+            int n=resu.size();
+            
+            for(int i=0;i<n;++i){
+                List<Integer> l=new ArrayList<>(resu.get(i));
+                
+                l.add(j);
+                resu.add(l);
+            }
+        }
+        
+        return resu;
     }
 }
