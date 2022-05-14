4207407c8d735c6fa31e6c70c58a0608becea399
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-14 18:38:52 +0530

    Time: 7 ms (81.73%), Space: 11 MB (53.70%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<vector<int>> res{};
     
-    void backtrack(vector<int> nums, int i, int sum, vector<int> v){
+    void backtrack(vector<int>& nums, int i, int sum, vector<int>& v){
+        /*
         if(i==nums.size()){
             if(sum==0)
             res.push_back(v);
@@ -16,6 +17,21 @@ public:
         }
         
         backtrack(nums, i+1, sum, v);
+        */
+        
+        if(sum<0)
+            return;
+        
+        if(sum==0){
+            res.push_back(v);
+            return;
+        }
+        
+        for(int j=i;j<nums.size();++j){
+            v.push_back(nums[j]);
+            backtrack(nums, j, sum-nums[j], v);
+            v.pop_back();
+        }
     }
     
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
