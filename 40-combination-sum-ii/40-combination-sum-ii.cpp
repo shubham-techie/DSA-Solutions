3a83db5c09fe2070051e41aced8ebac91dd145a2
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-17 14:35:30 +0530

    Time: 71 ms (8.33%), Space: 16.5 MB (11.28%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> res{};
+    
+    void backtrack(vector<int>& a, vector<int> v, int t, int i){
+
+//         if(i==a.size()){
+//             if(t==0)
+//                 res.push_back(v);
+//             return;
+//         }
+        
+//         v.push_back(a[i]);
+//         backtrack(a, v, t-a[i], i+1);
+//         v.pop_back();
+        
+//         backtrack(a, v, t, i+1);
+        
+        if(t==0){
+            res.push_back(v);
+            return;
+        }
+        
+        if(t<0)
+            return;
+        
+        for(int j=i;j<a.size();++j){
+            v.push_back(a[j]);
+            backtrack(a, v, t-a[j], j+1);
+            v.pop_back();
+            
+            while(j+1<a.size() && a[j]==a[j+1])
+                ++j;
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& a, int t) {
+        sort(a.begin(), a.end());
+        
+        vector<int> v{};
+        backtrack(a, v, t, 0);
+        return res;
+    }
+};
