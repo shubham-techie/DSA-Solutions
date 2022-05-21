7a7c5d2ed6e97ec1fe47460869d94a57e113cbdc
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-21 22:20:17 +0530

    Time: 5 ms (43.39%), Space: 8 MB (38.38%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -4,9 +4,8 @@ public:
     int n{};
     
     void backtrack(vector<int>& a, vector<int> v, vector<bool> b){
-        if(v.size()==n){
+        if(v.size()==n)
             res.push_back(v);
-        }
         
         for(int i=0;i<n;++i){
             if(!b[i]){
@@ -21,13 +20,28 @@ public:
         
     }
     
+    void bt(vector<int>& a, int i){
+        if(i==n)
+            res.push_back(a);
+        
+        for(int j=i;j<n;++j){
+            swap(a[j], a[i]);
+            bt(a, i+1);
+            swap(a[j], a[i]);
+        }
+    }
+    
     vector<vector<int>> permute(vector<int>& a) {
         n=a.size();
         
+        /*
         vector<int> v{};
         vector<bool> b(n, false);
         backtrack(a, v, b);
+        */
         
+        
+        bt(a, 0);
         return res;
     }
 };
