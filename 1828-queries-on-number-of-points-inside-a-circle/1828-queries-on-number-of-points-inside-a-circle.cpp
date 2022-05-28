a4f001a2cec83cac52853e1e4d45dbbb85a3d90b
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-28 08:41:33 +0530

    Time: 103 ms (83.78%), Space: 16.7 MB (27.68%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        sort(points.begin(), points.end());
         
         int n=queries.size(), i{-1};
         vector<int> res(n,0);
             
         for(auto& v:queries){
             int sum{};
-            
-            for(auto& p:points){
-                if(sqrt(pow(v[0]-p[0],2)+pow(v[1]-p[1], 2)) <=v[2])
+            auto it=lower_bound(points.begin(), points.end(), vector<int>{v[0]-v[2], 0});
+
+            for(; it!=points.end(); ++it){
+                if((*it)[0] > v[0]+v[2])
+                    break;
+                else if(pow((*it)[0]-v[0], 2) + pow((*it)[1]-v[1],2) <=pow(v[2], 2)){
                     ++sum;
+                }
+                
             }
             
             res[++i]=sum;
