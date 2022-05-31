7bfa422fc3e3033fc6246b87d576680138b4c270
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-05-31 22:35:08 +0530

    Time: 644 ms (32.07%), Space: 71.8 MB (42.77%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    unordered_map<string,bool> map{};
+    
+    void hashTable(string s, int k){
+        if(s.size()==k){
+            map[s]=false;
+            return;
+        }
+        
+        hashTable(s+'0',k);
+        hashTable(s+'1',k);
+    }
+    
+    bool hasAllCodes(string s, int k) {
+        /*
+        hashTable("", k);
+        
+        for(auto& m:map){
+            if(s.find(m.first)!=string::npos)
+                m.second=true;
+        }
+        
+         return !any_of(map.begin(), map.end(), [](const auto& m){
+            return m.second==false;
+        });
+        */
+        
+        unordered_set<string> set{};
+        int size=s.size();
+            
+        for(int i=0;i<=size-k;++i)
+            set.insert(s.substr(i, k));
+        
+        return set.size()==pow(2,k);
+    }
+};
