d457c87c9bba4d29aad32bb6afe03cd9ea4b25ed
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-06-01 20:52:13 +0530

    Time: 3 ms (96.76%), Space: 12.9 MB (99.16%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,19 +4,21 @@ public:
         int buyedStock{INT_MAX}, profit{0};
         int n=prices.size();
         
-//         for(int& i:prices){
-//             if(i>buyedStock){
-//                 profit+=(i-buyedStock);
-//                 buyedStock=i;
-//             }
-//             else
-//                 buyedStock=min(buyedStock, i);
-//         }
+        for(int& i:prices){
+            if(i>buyedStock){
+                profit+=(i-buyedStock);
+                buyedStock=i;
+            }
+            else
+                buyedStock=min(buyedStock, i);
+        }
         
+        /*
             for(int i=1;i<n;++i){
                 if(prices[i]>prices[i-1])
                     profit+=prices[i]-prices[i-1];
             }
+        */
         return profit;
     }
 };
