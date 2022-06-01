e1527282451eb6de7234e640ba09eb1cc609e5fa
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-06-02 00:35:44 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (61.90%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -3,26 +3,16 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
         
-        if(digits[n-1]<9){
-            ++digits[n-1];
-            return digits;
-        }
-        else if(count(begin(digits), end(digits),9)==n){
-            digits.clear();
-            digits.resize(n+1,0);
-            digits[0]=1;
-            return digits;
-        }
-        else{ //there is one digit which is not 9
-            for(auto it=digits.end();it--!=digits.begin();)
-                if(*it==9)
-                    *it=0;
-                else{
-                    ++*it;
-                    break;
-                }
+        for(auto it=digits.end();it--!=digits.begin();){
+            if(*it==9)
+                *it=0;
+            else if(*it<9){
+                ++*it;
+                return digits;
+            }
         }
         
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
