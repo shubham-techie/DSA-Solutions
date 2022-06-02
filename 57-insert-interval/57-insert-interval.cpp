dc28e19b4f148f1cb1d56f3c048a954ba9e831f6
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-06-02 20:37:13 +0530

    Time: 7 ms (99.53%), Space: 17.1 MB (75.48%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -4,23 +4,7 @@ public:
         int n=it.size();
         vector<vector<int>> v{};
 
-        if(n==0 || (nit[0]<=it[0][0] && nit[1]>=it[n-1][1])){
-            v.push_back(nit);
-            return v;
-        }
-        
-        if(nit[1]<it[0][0]){
-            it.insert(it.begin(),nit);
-            return it;
-        }
-
-        if(it[n-1][1]<nit[0]){
-            it.push_back(nit);
-            return it;
-        }
-            
-        
-        for(int k=0;k<n;++k){
+         for(int k=0;k<n;++k){
  
             if(nit[1]<it[k][0]){
                 v.push_back(nit);
