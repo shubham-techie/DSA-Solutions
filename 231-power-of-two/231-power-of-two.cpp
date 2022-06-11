62dbd91ec9e48dce8530a928bd11eae57cdd2045
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-06-11 15:18:28 +0530

    Time: 6 ms (18.10%), Space: 5.7 MB (98.97%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfTwo(int n) {
         
         if(n==1) return true;
-        if(n&1 || n==0) return false;
+        if(n&1 || n<=0) return false;
         return isPowerOfTwo(n/2);
     }
 };
