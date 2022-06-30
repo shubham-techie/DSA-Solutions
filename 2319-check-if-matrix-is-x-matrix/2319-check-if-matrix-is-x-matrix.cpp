26caf67bf113c3bc501fdaf17ed4be34ae10d7c5
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-06-30 10:07:10 +0530

    Time: 48 ms (65.87%), Space: 16.3 MB (45.30%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -5,15 +5,10 @@ public:
         
         for(int i=0;i<n;++i){
             for(int j=0;j<n;++j){
-                if(i==j){
+                if(!(i^j) || i+j==n-1){
                     if(grid[i][j]==0) 
                         return false;
                 }
-                else if(i+j==n-1){
-                    if(grid[i][j]==0) 
-                        return false;
-                }
-                
                 else {
                     if(grid[i][j]!=0) 
                         return false;
