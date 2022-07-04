976a7a4a91818dc03b932643050b93e3eb101f69
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-04 17:04:38 +0530

    Time: 12 ms (11.80%), Space: 7.7 MB (78.03%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -18,12 +18,27 @@ public:
         }
     }
     
+    void getPermutation(vector<int>& nums, vector<vector<int>>& v, int idx){
+        if(idx==nums.size()){
+            v.push_back(nums);
+            return;
+        }
+        
+        for(int i=idx;i<nums.size();++i){
+            swap(nums[idx], nums[i]);
+            getPermutation(nums, v, idx+1);
+            swap(nums[idx], nums[i]);
+        }
+    }
+    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> v{};
-        vector<bool> flag(nums.size());   //false means elements can be taken
-        vector<int> tmp{};
+//         vector<bool> flag(nums.size());   //false means elements can be taken
+//         vector<int> tmp{};
         
-        getPermutation(nums, v, flag, tmp);
+//         getPermutation(nums, v, flag, tmp);
+        
+            getPermutation(nums, v, 0);
         return v;
     }
 };
