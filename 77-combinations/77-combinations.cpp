a61993aeb6a0db1979c15ab64d5889fbe8858cb5
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-05 00:26:55 +0530

    Time: 96 ms (24.03%), Space: 43.3 MB (23.72%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helperCombination(int n, int k, int i, vector<int> subset, vector<vector<int>>& v){
+        if(k==0){
+            v.push_back(subset);
+            return;
+        }
+        
+        if(i==n+1) return;
+        
+        subset.push_back(i);
+        helperCombination(n, k-1, i+1, subset, v);
+        subset.pop_back();
+        
+        if(n-i>=k)
+            helperCombination(n,k, i+1,subset, v);
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> v{};
+        vector<int> subset{};
+        
+        helperCombination(n, k, 1, subset, v);
+        return v;
+    }
+};
