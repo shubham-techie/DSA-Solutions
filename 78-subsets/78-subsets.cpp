36e21ac09382356877c1c1e2f7f31611ead52bf7
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-04 12:24:58 +0530

    Time: 4 ms (54.28%), Space: 15.9 MB (22.59%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -17,10 +17,28 @@ public:
         }
     }
     
+    void generateSubsets(vector<int>& nums, vector<vector<int>>& v, vector<int> tmp, int idx){
+        if(idx==nums.size()){
+            v.push_back(tmp);
+            return;
+        }
+        
+        tmp.push_back(nums[idx]);
+        generateSubsets(nums, v, tmp, idx+1);
+        
+        tmp.pop_back();
+        generateSubsets(nums, v, tmp, idx+1);
+    }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> v{};
-        
-        generateSubsets(nums, v, 0);
+        vector<int> tmp{};
+        generateSubsets(nums, v, tmp, 0);
         return v;
     }
 };
+
+
+
+
+
