b5a39b37454d7de0a5c0e07337bcae71b727fb92
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-04 21:38:56 +0530

    Time: 21 ms (39.81%), Space: 10.9 MB (25.74%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void generatePermutations(vector<int> nums, vector<vector<int>>& v, int idx){
+    void generatePermutations(vector<int>& nums, vector<vector<int>>& v, int idx){
         if(idx+1==nums.size()) 
             v.push_back(nums);
         
-        // unordered_set<int> mp{};
+        unordered_set<int> mp{};
         
         for(int i=idx;i<nums.size();++i){
-            // if(mp.count(nums[i])) continue;
-            // mp.insert(nums[i]);
+            if(mp.count(nums[i])) continue;
+            mp.insert(nums[i]);
             
-            if(i!=idx && nums[i]==nums[idx]) continue;
-
             swap(nums[i], nums[idx]);
             generatePermutations(nums, v, idx+1);
-            // swap(nums[i], nums[idx]);
+            swap(nums[i], nums[idx]);
+            
         }
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(begin(nums), end(nums));
         
         vector<vector<int>> v{};
         generatePermutations(nums, v, 0);
