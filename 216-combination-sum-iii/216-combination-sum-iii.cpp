c357868f5810916a3a840eb7cf46646b9f62379b
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-05 13:14:02 +0530

    Time: 3 ms (50.16%), Space: 6.8 MB (33.62%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -8,12 +8,19 @@ public:
         
         if(sum<0 || size<0 || i>n) return;
         
-        subset.push_back(i);
-        help(sum-i, size-1, n, i+1, res, subset);
-        subset.pop_back();
+//         subset.push_back(i);
+//         help(sum-i, size-1, n, i+1, res, subset);
+//         subset.pop_back();
         
-        if(n-i>=size)
-            help(sum, size, n, i+1, res, subset);
+//         if(n-i>=size)   //if elements left are less than required size, then there is no point to generate further subsets
+//             help(sum, size, n, i+1, res, subset);
+        
+        
+        for(int j=i;j+size-1<=n;++j){
+            subset.push_back(j);
+            help(sum-j, size-1, n, j+1, res, subset);
+            subset.pop_back();
+        }
     }
     
     vector<vector<int>> combinationSum3(int size, int sum) {
