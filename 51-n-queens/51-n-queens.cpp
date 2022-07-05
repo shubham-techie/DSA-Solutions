213cfaff90addce023766a5d4b43f898d9b4b26e
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-05 19:38:41 +0530

    Time: 10 ms (58.85%), Space: 7.1 MB (95.67%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    bool isValidPos(int& n, int row, int col, vector<string>& board){
-        
-        //checking in row
-        for(int c=col;c>=0;--c)
-            if(board[row][c]=='Q')
-                return false;
-        
-        //checking in upper left diagonal
-        for(int r=row, c=col;r>=0 && c>=0;--r,--c)
-            if(board[r][c]=='Q')
-                return false;
-        
-        //checking in lower left diagonal
-        for(int r=row, c=col; r<n && c>=0; ++r, --c)
-             if(board[r][c]=='Q')
-                return false;
-        
-        //else
-        return true;
-    }
-    
-    void helperSolve(int& n, int col, vector<string>& board, vector<vector<string>>& res){
+    void helperSolve(int& n, int col, vector<string>& board, vector<vector<string>>& res, vector<int>& leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
         if(col==n){
             res.push_back(board);
             return;
         }
         
         for(int row=0;row<n;++row)
-            if(isValidPos(n, row, col, board)){
+            if(leftRow[row] & lowerDiagonal[row+col] & upperDiagonal[n-1-row + col]){
+                
                 board[row][col]='Q';
-                helperSolve(n, col+1, board, res);
+                leftRow[row] = lowerDiagonal[row+col] = upperDiagonal[n-1-row + col]=0;
+                
+                helperSolve(n, col+1, board, res, leftRow, upperDiagonal, lowerDiagonal);
+                
                 board[row][col]='.';
+                leftRow[row] = lowerDiagonal[row+col] = upperDiagonal[n-1-row + col]=1;
             }
     }
     
@@ -39,8 +23,10 @@ public:
         
         vector<string> board(n, string(n,'.'));
         vector<vector<string>> res{};
+
+        vector<int> leftRow(n,1), upperDiagonal((n<<1)-1,1), lowerDiagonal((n<<1)-1,1);   //1 : that cell can be considered
         
-        helperSolve(n, 0, board, res);
+        helperSolve(n, 0, board, res, leftRow, upperDiagonal, lowerDiagonal);
         return res;
     }
 };
