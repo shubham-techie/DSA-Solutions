022e37cf192e955692a8f89b999dbb96e2089702
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-05 11:53:20 +0530

    Time: 93 ms (26.37%), Space: 21.8 MB (30.17%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -8,11 +8,18 @@ public:
         
         if(target<0 || idx>=nums.size()) return;
         
-        subset.push_back(nums[idx]);
-        help(nums, target-nums[idx], res, subset, idx);
-        subset.pop_back();
+//         subset.push_back(nums[idx]);
+//         help(nums, target-nums[idx], res, subset, idx);
+//         subset.pop_back();
         
-        help(nums, target, res, subset, idx+1);
+//         help(nums, target, res, subset, idx+1);
+        
+        
+        for(int i=idx;i<nums.size();++i){
+            subset.push_back(nums[i]);
+            help(nums, target-nums[i],res, subset,i);
+            subset.pop_back();
+        }
     }
     
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
