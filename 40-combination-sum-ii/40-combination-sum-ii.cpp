5d8faee869ee7753d64c300c7c47fa1f59a669ae
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-05 12:33:56 +0530

    Time: 45 ms (12.19%), Space: 16.4 MB (12.86%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -8,13 +8,21 @@ public:
         
         if(t<0 || idx==nums.size()) return;
         
-        subset.push_back(nums[idx]);
-        help(nums, t-nums[idx], idx+1, res, subset);
-        subset.pop_back();
+//         subset.push_back(nums[idx]);
+//         help(nums, t-nums[idx], idx+1, res, subset);
+//         subset.pop_back();
         
-        while(idx+1<nums.size() && nums[idx]==nums[idx+1]) ++idx;
+//         while(idx+1<nums.size() && nums[idx]==nums[idx+1]) ++idx;
         
-        help(nums, t, idx+1, res, subset);
+//         help(nums, t, idx+1, res, subset);
+        
+        for(int i=idx;i<nums.size();++i){
+            if(i>idx && nums[i]==nums[i-1]) continue;
+            
+            subset.push_back(nums[i]);
+            help(nums, t-nums[i], i+1, res, subset);
+            subset.pop_back();
+        }
     }
     
     vector<vector<int>> combinationSum2(vector<int>& nums, int t) {
