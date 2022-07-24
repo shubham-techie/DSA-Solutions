742cbebddfbd3d0df71f233db3bbbcdf9987cbca
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-24 09:47:27 +0530

    Time: 54 ms (80.00%), Space: 70.9 MB (60.00%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,23 +1,15 @@
 class Solution {
-public:
-    int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<string,int> mp{};
-        int n=grid.size();
-        int cnt{};
+    public int equalPairs(int[][] grid) {
+        int cnt=0;
+        int n=grid.length;
         
-        for(auto i:grid){
-            string s="";
-            for(int& j:i)
-                s+=(j+'0');
-            ++mp[s];
-        }
-        
-        for(int i=0;i<n;++i){
-            string s="";
-            for(int j=0;j<n;++j)
-                s+=(grid[j][i]+'0');
-            cnt+= mp[s];
-        }
+        for(int[] row:grid)
+            for(int j=0, i=0;j<n;++j){
+                for(i=0;i<n;++i)
+                    if(grid[i][j]!=row[i])
+                        break; 
+                if(i==n) ++cnt;
+            }
         return cnt;
     }
-};
+}
