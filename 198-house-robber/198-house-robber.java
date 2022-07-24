0e412a96b267bb0fd4972312bcc43e7a10d0396c
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-24 20:53:16 +0530

    Time: 1 ms (20.84%), Space: 41.5 MB (54.46%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -4,17 +4,16 @@ class Solution {
         if(i==nums.length) return 0;
         if(i==nums.length-1) return nums[i];
         
-        if(dp[i+2]==-1) dp[i+2]=solve(nums, i+2, dp);
-        int pk= nums[i] + dp[i+2];
+        if(dp[i]!=-1) return dp[i];
         
-        if(dp[i+1]==-1) dp[i+1]=solve(nums, i+1, dp);
-        int npk = dp[i+1];
+        int pk= nums[i] + solve(nums, i+2, dp);
+        int npk = solve(nums, i+1, dp);
         
-        return Math.max(pk, npk);
+        return dp[i]= Math.max(pk, npk);
     }
     
     public int rob(int[] nums) {
-        int[] dp= new int[nums.length+1];
+        int[] dp= new int[nums.length];
         Arrays.fill(dp,-1);
         
        return solve(nums, 0, dp);
