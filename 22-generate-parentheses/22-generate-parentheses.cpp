a9a2bd00101d4e6555cdb6497996d978fdbd0747
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-07-26 22:29:15 +0530

    Time: 12 ms (23.29%), Space: 18.6 MB (8.46%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int cnt(string s, char c){
+        return count(begin(s), end(s), c);
+    }
+    
+    void generate(int n, string s, vector<string>& res){
+        int lp=cnt(s,'(');
+        int rp=cnt(s, ')');
+        
+        if(rp>n || lp>n || rp>lp)
+            return;
+        
+        if(lp==n && rp==n){
+            res.push_back(s);
+            return;
+        }
+        
+        generate(n, s+'(', res);
+        generate(n, s+')', res);
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generate(n, "", res);
+        return res;
+    }
+};
