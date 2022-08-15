32db7a1b81d095e36842277336bec2f654b9bbe4
Shubham Jaiswal <85562020+shubham-techie@users.noreply.github.com>
2022-08-15 21:09:30 +0530

    Time: 154 ms (15.26%), Space: 47.4 MB (5.24%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,68 +1,14 @@
 class Solution {
 public:
-    int partition(vector<int>& v, int left, int right){
-        
-        int i{left},pivot{v[right]};
-        
-        for(int j=left;j<right;++j){
-            if(v[j]>=pivot){
-                swap(v[j],v[i]);
-                ++i;
-            }
-        }
-        swap(v[i],v[right]);
-        return i;
-    }
-    
-    int quickSelect(vector<int>& nums, int k,int left,int right){
-         
-        int idx{partition(nums,left,right)};
-     
-            if(idx==k-1)
-                return nums[idx];
-        
-            if(idx>k-1)
-               return quickSelect(nums,k, left,idx-1);
-            
-            return quickSelect(nums,k, idx+1,right);
-        
-        return INT_MIN;
-    }
-    
     int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minhp{};
         
-        /* Method 1: Sorting and finding kth ele
-
-        sort(begin(nums),end(nums));
-        return nums[nums.size()-k];
-        */
-        
-        
-        /* Method 2: using mapping
-        map<int,int> m;
-        
-        for(int& i:nums)
-            ++m[i];
-        
-        for(auto it=m.rbegin();it!=m.rend();++it){
-            k-=it->second;
+        for(int& i:nums){
+            minhp.push(i);
             
-            if(k<=0)
-                return it->first;
+            if(minhp.size()>k)
+                minhp.pop();
         }
-        
-        return -1;
-        */
-        
-        
-        /* Method 3: STL nth_element()
-        
-        nth_element(nums.begin(),nums.begin()+nums.size()-k,nums.end());
-        return nums[nums.size()-k];
-        */
-        
-        
-        // Method 4: Quick Select
-       return quickSelect(nums,k,0 ,nums.size()-1);
+        return minhp.top();
     }
 };
